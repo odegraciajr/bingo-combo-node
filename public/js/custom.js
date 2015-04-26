6a7fbc73850f3de73396da59e9e6924d0daedaf0
Oscar De Gracia Jr <odegraciajr@gmail.com>
2015-04-27 05:05:00 +0800

    popup added

---
+++ public/js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+	
+	$('#add-bet-btn').click(function(){
+		$('#mod-add-bet').modal('show');
+	});
+	$('#mod-add-bet').on('show.bs.modal', function (e) {
+		$(this).center();
+	});
+});
+jQuery.fn.center = function () {
+	this.css("position","absolute");
+	this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+												$(window).scrollTop()) + "px");
+	this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+												$(window).scrollLeft()) + "px");
+	return this;
+}
